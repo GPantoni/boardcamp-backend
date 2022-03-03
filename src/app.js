4aa0a6810e01eb7192b1f4c41a5f8ed32ee10426
Glauco Pantoni <glaucopantoni@gmail.com>
2022-03-03 16:22:32 -0300

    Add app and database configuration

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+// app.use(router);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on PORT ${process.env.PORT}`)
+);
