970c9fa2cba89f4733c568d605dfd0a0fb993b90
mateuskuritza <mateuskuritza@gmail.com>
2022-03-11 14:50:33 -0300

    Preparando revisão de código

--- src/schemas/insertRentalSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const insertRentalSchema = Joi.object({
-  customerId: Joi.number().integer().required(),
-  gameId: Joi.number().integer().required(),
-  daysRented: Joi.number().integer().greater(0).required(),
-});
-
-export default insertRentalSchema;
